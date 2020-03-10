f5ce737940e0995bbf38b7d308127cc8047a0429
Pierre <pierre.maruejol@etu.umontpellier.fr>
2020-03-10 14:48:43 +0100

    init

--- js/script.js
+++ js/script.js
@@ -4,3 +4,49 @@ document.body.onresize = function resize() {
   document.getElementById("bgp").style.height = 1080 * (window.innerWidth / 1920) - 5 + "px";
 }
 
+'use strict';
+
+const textWriter = (()=>{
+
+  const intro = document.querySelector('.intro');
+
+  const arr = [];
+  let value = intro.firstChild.nodeValue;
+  let current = 0;
+  let intervalTime = 0;
+
+  const text = () =>{
+    for (let i = 0, len = value.length; i < len; i++){
+      arr.push(value.charAt(i));
+    }
+
+    intro.firstChild.nodeValue = '';
+
+    const textWriter = () =>{
+      intro.firstChild.nodeValue += arr[current];
+      current++;
+      if (current === arr.length){
+        clearInterval(duration);
+      }
+
+    };
+
+    const duration = setInterval(textWriter, intervalTime);
+  };
+
+  const loadFunction = (callback) =>{
+    if (addEventListener){
+      window.addEventListener('load', callback, false);
+    }
+  };
+
+  return {
+    init: (time) =>{
+      intervalTime = time;
+      loadFunction(text);
+    }
+  };
+
+})();
+
+textWriter.init(100);
