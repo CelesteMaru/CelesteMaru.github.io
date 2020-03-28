4412312c3c99e05ca8fc3f69a28ac93465bf0e39
Grochocrum <pierre.maruejol@etu.umontpellier.fr>
2020-03-28 19:25:43 +0100

    FIN

--- js/script.js
+++ js/script.js
@@ -101,7 +101,6 @@ $('#boop').on("click", function(e) {
 
 
 
-
 class NoiseMap {
   constructor(width, height) {
     //Dimensions
@@ -384,4 +383,101 @@ $(function() {
 
 $(function() {
   $('[data-toggle="popover"]').popover()
-})
+});
+
+
+var Boid = function(x, y, angle) {
+
+  this.x = x;
+  this.y = y;
+
+  this.angle = Math.pow(Math.random(), 20) + angle;
+  this.dx = Math.cos(this.angle);
+  this.dy = Math.sin(this.angle);
+
+  this.life = Math.random() * 100 + 100;
+  this.dead = false;
+
+  this.update = function() {
+
+    context.strokeStyle = '#000000';
+    context.beginPath();
+    context.moveTo(this.x, this.y);
+
+    this.x += this.dx * 2;
+    this.y += this.dy * 2;
+    this.life -= 2;
+
+    context.lineTo(this.x, this.y);
+    context.stroke();
+
+    var index = (Math.floor(this.x) + width * Math.floor(this.y)) * 4;
+
+    if (this.life <= 0) this.kill();
+    if (data[index + 3] > 0) this.kill();
+
+    if (this.x < 0 || this.x > width) this.kill();
+    if (this.y < 0 || this.y > height) this.kill();
+
+  }
+
+  this.kill = function() {
+
+    boids.splice(boids.indexOf(this), 1);
+    this.dead = true;
+
+  }
+
+}
+
+var width = window.innerWidth;
+var height = window.innerHeight / 4;
+
+var canvas = document.getElementById('world');
+canvas.width = width;
+canvas.height = height;
+
+var context = canvas.getContext('2d');
+var image, data;
+
+var boids = [];
+
+
+setInterval(function() {
+
+  image = context.getImageData(0, 0, width, height);
+  data = image.data;
+
+  for (var i = 0; i < boids.length; i++) {
+
+    var boid = boids[i];
+    boid.update();
+
+    if (!boid.dead && Math.random() > 0.5 && boids.length < 500) {
+
+      boids.push(new Boid(boid.x, boid.y, (Math.random() > 0.5 ? 90 : -90) * Math.PI / 180 + boid.angle));
+
+    }
+
+  }
+
+}, 1000 / 60);
+
+el = document.getElementById("world");
+boolbal = false;
+
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+
+  return rect.bottom > 0 &&
+    rect.right > 0 &&
+    rect.left < (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */ &&
+    rect.top < (window.innerHeight || document.documentElement.clientHeight) /* or $(window).height() */ ;
+}
+
+document.body.onscroll = function() {
+  if (isElementInViewport(el) && !boolbal) {
+    boids.push(new Boid(width / 2, height / 2, Math.random() * 360 * Math.PI / 180));
+    boolbal = true;
+  }
+}
