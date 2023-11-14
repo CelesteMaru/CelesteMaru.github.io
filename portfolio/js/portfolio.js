a9ace41aa8b26fc8b9dc00fda25d8d5ce06519e0
PierreMARUEJOL <pierre.maruejol@gmail.com>
2023-11-14 14:20:43 -0500

    Added inktober2023 except day1 Added hover effect on the card Added a carousel that opens a card is clicked

--- portfolio/js/portfolio.js
+++ portfolio/js/portfolio.js
@@ -1,69 +1,74 @@
-const folders = ["inktober2022"];
-//const file = [["1Gargoyle..png\n"+, "2Scurry..png\n"+", "3Bat..png\n"+"]];
+const folders = ["inktober2022","inktober2023"];
 const imageDiv = document.getElementById("images");
 
-const testText = `1Gargoyle.png
-2Scurry.png
-3Bat.png
-3BatSketch.png
-4Scallop.png
-6Bouquet.png
-7Trip.png
-8Match.png
-9Nest.png
-10Crabby.png
-11Eagle.png
-12Forget.png
-13Kind.png
-14Empty.png
-15Armadillo.png
-16Fowl.png
-17Salty.png
-18Scrape.png
-19Ponytail.png
-20Bluff.png
-21BadDog.png
-22Heist.png
-23Booger.png
-24Fairy.png
-25Tempting.png
-26Ego.png
-27Snack.png
-28Camping.png
-29UH-OH.png
-30Gear.png
-31Farm.png`
-
-
-displayCard = (foldername, image)=>{
-    return `<div class="col-sm-12 col-md-6 col-lg-3 h-100" style="margin-bottom:1%;">
-    <div class="card bg-dark h100" style="padding:0;">
-    
-        <div class="row w-100 h-100" style="margin:0;">
-            <img src="./portfolio/ressources/${foldername}/${image}" alt="${image}" style="height:12.5rem; width:auto; padding:0; display:block; margin-left:auto;margin-right:auto;">
-            <div class="row align-items-end h-100 w-100">
-                <div class="position-absolute" style="background-color: rgba(0,0,0,0.5); height:20%; width:100%; padding:0;">
-                    <h4 class="card-title" style="color: rgba(255,255,255,1); margin-left:2%">${image}</h5>
+displayCard = (foldername, image, imageIndex)=>{
+    return `<div class="col-sm-12 col-md-6 col-lg-3 h-100 card-container hoverable">
+                <div class="card h100">
+                    <div class="row w-100 h-100" style="margin:0;">
+                        <img src="./portfolio/ressources/${foldername}/${image}" onclick="goToSlide(${imageIndex})" alt="${image}" class="card-img">
+                        <div class="row align-items-end h-100 w-100">
+                            <div class="position-absolute overlay">
+                                <h4 class="card-title">${image}</h5>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    </div>
-    </div>`
+            </div>`
 }
+createCarouselSlide = (foldername,image)=>{
+    const item = document.createElement("div");
+    item.classList.add("carousel-item");
+
+    const img = document.createElement("img");
+    img.src = `./portfolio/ressources/${foldername}/${image}`;
+    img.alt = image;
+    img.classList.add("d-block" ,"carousel-image");
+
+    item.appendChild(img);
+    return item;
+}
+
 renderPhoto = (sourceFolder,array)=>{
-    array.forEach(image => {
-        imageDiv.innerHTML+= displayCard(sourceFolder,image);
+    array.forEach((image, index) => {
+        // ---- Vignette Display ----
+        imageDiv.innerHTML+= displayCard(sourceFolder,image,imageIndex);
+        imageIndex++;
+
+        // ---- Carousel ----
+        const carouselSlide = createCarouselSlide(sourceFolder,image);
+        // Activate the first slide
+        if (index === 0 && sourceFolder==folders[0]) {
+            carouselSlide.classList.add("active");
+        }
+        carouselInner.appendChild(carouselSlide);
     });
 }
 
-//renderPhoto(testText.split("\n"));
+// When we click on card we open the carousel on this image
+goToSlide = (index)=>{
+    carouselDOM.style.display="flex";
+    bootstrapCarousel.to(index);
+}
 
-fetch("portfolio/ressources/inktober2022/file.txt")
-.then((res) => res.text())
-.then((text) => {
-  renderPhoto("inktober2022",text.split("\n"));
- })
-.catch((e) => console.error(e));
+// Code to close the carousel
+closeCarousel = ()=>{
+    carouselDOM.style.display="none";
+}
+
+
+const carouselDOM = document.getElementById("carousel-drawings");
+const carouselInner = document.getElementById("carousel-drawings-inner");
+const bootstrapCarousel = new bootstrap.Carousel("#carousel-drawings");
+
+let imageIndex = 0;
+folders.forEach(folder => {
+    fetch(`portfolio/ressources/${folder}/file.txt`)
+    .then((res) => res.text())
+    .then((text) => {
+        renderPhoto(folder,text.split("\n"));
+    })
+    .catch((e) => console.error(e));
+});
 
 
 
